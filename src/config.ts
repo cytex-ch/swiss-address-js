f5bc0489e8022c05253be24f0bc17288bd27d960
Simon Jutz <simon.jutz@cytex.ch>
2023-08-31 08:38:26 +0200

    fix: use proper env variables

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,11 @@
 import 'dotenv/config';
 
-if (!(process.env.SWISSPOST_CLIENT_ID && process.env.SWISSPOST_CLIENT_SECRET)) {
+if (
+  !(
+    process.env.SWISSPOST_ADDRESS_WEB_SERVICES_USERNAME &&
+    process.env.SWISSPOST_ADDRESS_WEB_SERVICES_PASSWORD
+  )
+) {
   // istanbul ignore next
   throw new Error('Missing environment variables');
 }
